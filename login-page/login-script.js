fff74c1a5daca7cb3f775717ef8b1f4ddf302f14
André Macedo <asilvamacedo@gmail.com>
2022-10-03 13:36:29 +0100

    added the transition for the logo on hover

--- login-page/login-script.js
+++ login-page/login-script.js
@@ -46,3 +46,8 @@ function generateBurgerMenu(el) {
 		  });
 	burgerMenuOpen.appendChild(linksContainer);
 }
+const name = 'John Doe';
+const age = 26;
+const passions = ['Ski', 'Hiking', 'Cinema'];
+
+console.log(name, age, passions)
