418e5a97f0c04bd421d00f26a90cb467b69044da
Orenikoe <orental21@gmail.com>
2022-10-04 15:03:32 +0100

    centrlized data in one js file

--- about-us-page/script-about-us.js
+++ about-us-page/script-about-us.js
@@ -1,23 +1,4 @@
-const teamData = [
-	{
-		name: 'Gonçalo Da Silva',
-		title: 'CEO',
-		description: `enjoyiing journalizm since 8 years. very dedicted to my job. when i'm not reading - i like to play tennis.`,
-		image: 'assets/1517063149400.jpg',
-	},
-	{
-		name: 'André Macedo',
-		title: 'CTO',
-		description: `enjoyiing journalizm since 8 years. very dedicted to my job. when i'm not reading - i like to play tennis.`,
-		image: 'assets/andre-pic.jpg',
-	},
-	{
-		name: 'Oren Tal',
-		title: 'Admnistration Officer',
-		description: `enjoyiing journalizm since 8 years. very dedicted to my job. when i'm not reading - i like to play tennis.`,
-		image: 'assets/oren-pic.jpg',
-	},
-];
+import {teamData} from '../data.js'
 const cardsWrapper = document.querySelector('main');
 
 
---
+++ data.js
@@ -0,0 +1,20 @@
+export const teamData = [
+	{
+		name: 'Gonçalo Da Silva',
+		title: 'CEO',
+		description: `enjoyiing journalizm since 8 years. very dedicted to my job. when i'm not reading - i like to play tennis.`,
+		image: 'assets/1517063149400.jpg',
+	},
+	{
+		name: 'André Macedo',
+		title: 'CTO',
+		description: `enjoyiing journalizm since 8 years. very dedicted to my job. when i'm not reading - i like to play tennis.`,
+		image: 'assets/andre-pic.jpg',
+	},
+	{
+		name: 'Oren Tal',
+		title: 'Admnistration Officer',
+		description: `enjoyiing journalizm since 8 years. very dedicted to my job. when i'm not reading - i like to play tennis.`,
+		image: 'assets/oren-pic.jpg',
+	},
+];
