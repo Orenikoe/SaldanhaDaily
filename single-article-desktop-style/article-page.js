149db2663888dcda77f11512ef7e931c048fda7e
Gonçalo Silva <goncalo_dsilva@hotmail.com>
2022-10-05 22:18:20 +0100

    Created js files for articles

--- single-article-desktop-style/article-page.js
+++ single-article-desktop-style/article-page.js
@@ -2,9 +2,32 @@ import {sideArticlesData} from '../single-article-desktop-style/article-data'
 
 const sidePosts = document.querySelector(".sidePosts");
 
+function createArticle(name, imageUrl){
+    const posts = document.createElement("div");
+    posts.classList.add("posts");
+    sidePosts.appendChild(posts);
 
+    const postsHeader = document.createElement("div");
+    postsHeader.classList.add("posts-header");
+    posts.appendChild(postsHeader);
 
-function createArticle(title, image){
+    const postsImage=document.createElement("div");
+    postsImage.style.backgroundImage=`url${imageUrl}`;
+    postsImage.classList.add("posts-img");
+    postsHeader.appendChild(postsImage);
 
+    const postsBody = document.createElement("div");
+    postsBody.classList.add("posts-body");
+    posts.appendChild(postsBody);
+
+    const cardTitle = document.createElement("h2");
+    cardTitle.classList.add("posts-title");
+    cardTitle.innerText = name;
+    posts.appendChild(cardTitle);
 
 }
+
+for (let i = 0; i < sideArticlesData.length; i++) {
+    createArticle(sideArticlesData[i].name, sideArticlesData[i].imageUrl);
+  }
+
